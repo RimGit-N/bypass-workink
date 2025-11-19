e9c983d1a23a8900628942ef7e8bde825125e05c
RimGit-N <72115346+RimGit-N@users.noreply.github.com>
2025-11-19 18:28:53 +0700

    Add files via upload

---
+++ bypass.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Workink and Volcano bypass
+// @namespace    http://tampermonkey.net/
+// @version      3.0.3.0
+// @description  bypasss workink and volcano key system
+// @author       Dyrian and elfuhh
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @run-at       document-start
+// @grant        unsafeWindow
+// @icon         https://cdn.discordapp.com/icons/1312680907559276576/bedc7397e04d863a9ee5c6087ece8ae8.png?size=80&quality=lossless
+// @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @require https://github.com/elfuhh/bypass/raw/refs/heads/main/bypass.js
+// ==/UserScript==
